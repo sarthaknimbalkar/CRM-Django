fb5029c27453a5250cc4d904107e1d0ef1197171
Sarthak <54149390+sarthaknimbalkar@users.noreply.github.com>
2023-07-28 14:29:52 +0530

    Add files via upload

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="##SSN-2021s",
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE SarthakCo")
+
+print("Okay Done!")
